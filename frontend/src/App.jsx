e9940ff071e9021209136666221c1cc44f9e2466
Aashutosh31 <aashutosh.bairagi05@gmail.com>
2025-11-13 13:22:18 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-center bg-blue-500 text-white p-4 rounded'>Hello, MERN Stack!</h1>
+    </>
+  )
+}
+
+export default App
